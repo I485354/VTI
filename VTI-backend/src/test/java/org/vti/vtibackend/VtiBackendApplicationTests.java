2a01430861bb27ae23bdac002da42f7dd38a5cc8
Selattin Fidan <485354@student.fontys.nl>
2024-10-27 15:10:04 +0100

    package.json upgedate en de test

--- VTI-backend/src/test/java/org/vti/vtibackend/VtiBackendApplicationTests.java
+++ VTI-backend/src/test/java/org/vti/vtibackend/VtiBackendApplicationTests.java
@@ -3,7 +3,7 @@ package org.vti.vtibackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 class VtiBackendApplicationTests {
 
     @Test

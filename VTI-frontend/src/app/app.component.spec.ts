47c6462920dc5d36e047ea9bdf951508d0de0b3c
Selattin Fidan <485354@student.fontys.nl>
2024-12-11 10:57:06 +0100

    linting

--- VTI-frontend/src/app/app.component.spec.ts
+++ VTI-frontend/src/app/app.component.spec.ts
@@ -13,14 +13,7 @@ describe('AppComponent', () => {
     // Controleer of het component correct is geladen
     cy.get('app-root').should('exist');
   });
-
-  it(`should have the title 'VTI-frontend'`, () => {
-    // Controleer of de titel 'VTI-frontend' in de component aanwezig is
-    cy.window().its('app').then((app: any) => {
-      expect(app.title).to.equal('VTI-frontend');
-    });
-  });
-
+  
   it('should render title', () => {
     // Controleer of de titel correct wordt weergegeven in de HTML
     cy.contains('VTI-frontend').should('be.visible');

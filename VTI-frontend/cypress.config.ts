2c13820e8822e5295c495074defe2c988fd49ba9
Selattin Fidan <485354@student.fontys.nl>
2024-12-19 01:55:21 +0100

    url veranderingen in cypress.config.ts

--- VTI-frontend/cypress.config.ts
+++ VTI-frontend/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:4200",
+    baseUrl: "https://vti-frontend-hnaavycex-i485354s-projects.vercel.app",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
